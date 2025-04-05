fd17cbd7c9911bab6d9e7e8150455de539444a32
Maj3D.10 <hadfi.madjed@gmail.com>
2025-04-05 19:52:31 +0100

    Added Category and Product Models

---
+++ Fuzzy.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fuzzy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
